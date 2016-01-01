68f411caad70ca025a86031240e055209be6bfe0
Diana Chang <diana.chang.twn@gmail.com>
2016-01-01 00:04:48 -0500

    Create codeIn2.py

---
+++ codeIn2.py
@@ -0,0 +1,10 @@
+name = input("What is your name?")
+weight = input("How much do you weigh in kg?")
+height = input("How tall are you in cm?")
+BMI = weight/((height/100)**2)
+if BMI>25:
+  print("You're overweight with a BMI of",BMI)
+elif (BMI>=18.5):
+  print("You're doing great with a BMI of",BMI)
+else:
+  print("You're underweight with a BMI of",BMI)
